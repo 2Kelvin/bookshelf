a36ff60cb5345c5f9192289584b3e77c3c1836a9
2Kelvin <rocketmancodes02@gmail.com>
2024-03-24 18:51:34 +0300

    create a db and table in mysql

--- backend/index.js
+++ backend/index.js
@@ -5,34 +5,67 @@ import mysql from 'mysql';
 // creating my express server
 const app = express();
 
-// connecting to mysql database
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'bookstore-app',
+// connecting to mysql
+const sqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
 });
+
+// creating 'bookstore' db
+const createDbQuery = 'CREATE DATABASE IF NOT EXISTS bookshelf';
+sqlConnection.query(createDbQuery, (err, result) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('bookshelf database created successfully:', result);
+});
+
+// connecting to mysql 'bookstore' database
+const bookshelfDb = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bookshelf',
+});
+
+// creating 'books' table in bookstore db
+const createTableQuery = 'CREATE TABLE IF NOT EXISTS books (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(80) NOT NULL, description VARCHAR(255) NOT NULL, cover VARCHAR(80) NULL)';
+bookshelfDb.query(createTableQuery, (err, result) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('books table created successfully:', result);
+});
+
 
 // home route
 app.get('/', (req, res) => {
-  res.json('This is the backend');
+    res.json('This is the backend');
 });
 
-// books route
+// books routes
 // returning all books in book-app db
 app.get('/books', (req, res) => {
-  const booksQuery = 'SELECT * FROM books';
+    const getBooksQuery = 'SELECT * FROM books';
 
-  db.query(booksQuery, (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
+    bookshelfDb.query(getBooksQuery, (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    });
 });
+
+// creating a new book into bookshelf.books table
+app.post('/books', (req, res) => {
+    
+})
 
 // express server application listening on port 3000
 app.listen(8800, () => {
-  console.log('********** Backend connected (Express server is running on port 8800) ********** ');
+    console.log('********** Backend connected (Express server is running on port 8800) ********** ');
 });
 
-// RESUME FROM 13:00
-// YOU NEED TO CREATE THE DATABASE (bookstore-app) & TABLE (books) BEFORE app.get & app.post
+// enter this code in mysql to clear auth msql error
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
