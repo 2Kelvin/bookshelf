6f6b1cd3a644fee40551ffc25337e098953d0738
2Kelvin <rocketmancodes02@gmail.com>
2024-03-24 22:41:14 +0300

    displaying all my books on the browser

--- frontend/src/pages/Books.js
+++ frontend/src/pages/Books.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function Books() {
-    // books contains the all the books fetched from the api
+    // books contains the all the books fetched from the api (array of book objects)
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
@@ -10,7 +10,8 @@ export default function Books() {
         async function fetchAllBooks() {
             try {
                 const res = await axios.get('http://localhost:8800/books');
-                console.log(res);
+                // res.data contains the array of book objects
+                setBooks(res.data);
             } catch (err) {
                 console.error(err);
             }
@@ -21,7 +22,20 @@ export default function Books() {
 
     return (
         <div>
-            Books
+            <h1>The Bookshelf</h1>
+            <div className='books'>
+                {
+                    // books contains res.data (array of book objects)
+                    books.map((book) => (
+                        <div className='book' key={book.id}>
+                            {book.cover && <img src={book.cover} alt='book' />}
+                            <h2>{book.title}</h2>
+                            <p>{book.description}</p>
+                            <span>{book.isAvailable}</span>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 }
